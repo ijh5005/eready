f91ec8a5208740ba78dc0e5ceba80dc343eac393
ijh5005 <ijh5005@outlook.com>
2017-04-22 18:04:22 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -3,5 +3,12 @@
 var app = angular.module("app", []);
 
 app.controller("ctrl", [ "$scope", function ($scope) {
-	$scope.name = "rdysocial";
+	$scope.name = "Entertainment Ready";
+	$scope.homePageOptions = [ 
+		{name: "Upcoming Events", id: "upcoming"}, 
+		{name: "Previous Events", id: "previous"}, 
+		{name: "Memberships", id: "membership"}, 
+		{name: "Contact Us", id: "contact"}
+		];
+	console.log($scope.homePageOptions);
 }]);
