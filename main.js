d4cf4a0588536bb3031b880104bce483940b05e1
ijh5005 <ijh5005@outlook.com>
2017-04-23 15:37:45 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,8 @@
 
 var app = angular.module("app", []);
 
-app.controller("ctrl", [ "$scope", function ($scope) {
+app.controller("ctrl", [ "$scope", "nextPage", "prevPage", 
+						function ($scope, nextPage, prevPage) {
 	$scope.name = "Entertainment Ready";
 	$scope.homePageOptions = [ 
 		{name: "Upcoming Events", id: "upcoming"}, 
@@ -10,5 +11,94 @@ app.controller("ctrl", [ "$scope", function ($scope) {
 		{name: "Memberships", id: "membership"}, 
 		{name: "Contact Us", id: "contact"}
 		];
-	console.log($scope.homePageOptions);
+	$scope.nextPage = ($event) => {
+		nextPage.slide($event);
+	};
+	$scope.prevPage = ($event) => {
+		console.log("prev");
+		prevPage.slide($event);
+	};
 }]);
+
+app.service("nextPage", function (){
+	this.slide = ($event) => {
+		//cache the id of the page -> used to navigate to appropriate page
+		const id = $event.target.id;
+		const color = $("#" + id).css("backgroundColor");
+
+		const slidePage = () => {
+			$("#container").animate({
+				right: "110vw"
+			}, {
+				duration: 500
+			});
+
+			$("#dynamicPageOne").css("backgroundColor", color).fadeIn();
+		}
+
+		if(id == "titleBar"){
+			slidePage();
+		} else if(id == "upcoming"){
+			//slidePage();
+		} else if(id == "previous"){
+			//slidePage();
+		} else if(id == "membership"){
+			//slidePage();
+		} else if(id == "contact"){
+			//slidePage();
+		}
+	}
+});
+
+app.service("prevPage", function ($timeout){
+	this.slide = ($event) => {
+		//cache the id of the page -> used to navigate to appropriate page
+		//const id = $event.target.id;
+		//const color = $("#" + id).css("backgroundColor");
+		// console.log("prev");
+		const slidePage = () => {
+			//$("#dynamicPageOne").css("backgroundColor", "");
+			$("#container").animate({
+				right: "0vw"
+			}, {
+				duration: 500
+			});
+
+			$("#dynamicPageOne").fadeOut(800);
+			
+		}
+
+		slidePage();
+
+		// if(id == "titleBar"){
+		// 	slidePage();
+		// } else if(id == "upcoming"){
+		// 	slidePage();
+		// } else if(id == "previous"){
+		// 	slidePage();
+		// } else if(id == "membership"){
+		// 	slidePage();
+		// } else if(id == "contact"){
+		// 	slidePage();
+		// }
+	}
+});
+
+app.directive("homepage", function () {
+	return {
+		template: "	<div id=\"container\">"+
+						"<div id=\"titleBar\" class=\"nameBar\" ng-click=\"nextPage($event)\">"+
+							"{{name}}"+
+						"</div>"+
+						"<div ng-repeat=\"x in homePageOptions\" ng-click=\"nextPage($event)\" id=\"{{x.id}}\" class=\"mainMenuOption\">"+
+							"<div> {{x.name}} </div>"+
+						"</div>"+
+					"</div>"
+	}
+});
+
+app.directive("moreInfo", function () {
+	return {
+		template: "more info"
+	}
+});
